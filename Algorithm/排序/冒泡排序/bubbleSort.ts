84375718ee10d55f65eda0719e9f6f612e1fe41d
Diablo <750108747@qq.com>
2020-10-30 11:30:19 +0800

    feat(Algorithm): 排序--冒泡排序 简单的number类型排序（JavaScript + TypeScript）

---
+++ Algorithm/排序/冒泡排序/bubbleSort.ts
@@ -0,0 +1,15 @@
+/**
+ * 冒泡排序
+ * @param {Array} sortArray
+ * @return {Array} 排序后的数组
+ */
+function bubbleSort <T>(sortArray: T[]): T[] {
+  for(let i = 0; i < sortArray.length; i++) {
+    for(let j = 0; j < sortArray.length-i-1; j++) {
+      if(sortArray[j] > sortArray[j+1]) {
+        [sortArray[j],sortArray[j+1]] = [sortArray[j+1],sortArray[j]];
+      }
+    }
+  }
+  return sortArray;
+}
