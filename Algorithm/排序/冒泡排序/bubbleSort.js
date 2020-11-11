2d1b731f83d7d8e9f42f604b90a2042b8dc30ee0
Diablo <750108747@qq.com>
2020-11-11 13:02:39 +0800

    fix(quickSort): 修复快排堆栈溢出 1. 添加ts调试； 2. 修复快排堆栈溢出；

--- Algorithm/排序/冒泡排序/bubbleSort.js
+++ Algorithm/排序/冒泡排序/bubbleSort.js
@@ -4,12 +4,12 @@
  * @return {Array} 排序后的数组
  */
 function bubbleSort(sortArray) {
-  for(let i = 0; i < sortArray.length; i++) {
-    for(let j = 0; j < sortArray.length-i-1; j++) {
-      if(sortArray[j] > sortArray[j+1]) {
-        [sortArray[j],sortArray[j+1]] = [sortArray[j+1],sortArray[j]];
-      }
+    for (let i = 0; i < sortArray.length; i++) {
+        for (let j = 0; j < sortArray.length - i - 1; j++) {
+            if (sortArray[j] > sortArray[j + 1]) {
+                [sortArray[j], sortArray[j + 1]] = [sortArray[j + 1], sortArray[j]];
+            }
+        }
     }
-  }
-  return sortArray;
+    return sortArray;
 }
--- Algorithm/排序/快速排序/quickSort.ts
+++ Algorithm/排序/快速排序/quickSort.ts
@@ -5,17 +5,18 @@
  * @param {Number} hight 
  */
 function quickSort(sortArray: number[], low?: number, hight?: number) {
-  if(low >= hight) return; // 左右指针相等，退出排序
+  if(low >= hight) return sortArray; // 左右指针相等，退出排序
   // 指定左右指针
-  let i = low, j = hight;
-  let key = sortArray[low]; // 用子表的第一个记录做基准
-  while (i < j) { // 从表的两端交替向中间扫描
-      while (i < j && sortArray[j] >= key) j--;
-      if (i < j) sortArray[i++] = sortArray[j];// 用比基准小的记录替换低位记录
-      while (i < j && sortArray[i] < key) i++;
-      if (i < j) sortArray[j--] = sortArray[i]; // 用比基准大的记录替换高位记录
+  let left = low = low || 0, right = hight = hight || sortArray.length-1;
+  let pivot = sortArray[left]; // 用子表的第一个记录做基准
+  while (left < right) { // 从表的两端交替向中间扫描
+      while (left < right && sortArray[right] >= pivot) right--;
+      if (left < right) sortArray[left++] = sortArray[right];// 用比基准小的记录替换低位记录
+      while (left < right && sortArray[left] < pivot) left++;
+      if (left < right) sortArray[right--] = sortArray[left]; // 用比基准大的记录替换高位记录
   }
-  sortArray[i] = key;// 将基准数值替换回 sortArray[i]
-  quickSort(sortArray, low, i - 1); // 对低子表进行递归排序
-  quickSort(sortArray, j + 1, hight); // 对高子表进行递归排序
+  sortArray[left] = pivot;// 将基准数值替换回 sortArray[i]
+  quickSort(sortArray, low, left - 1); // 对低子表进行递归排序
+  quickSort(sortArray, right + 1, hight); // 对高子表进行递归排序
+  return sortArray;
 }
