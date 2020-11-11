275c0ed18a6cac846891bbeb11b6d102e839a156
Diablo <750108747@qq.com>
2020-11-11 10:04:35 +0800

    feat(quickSort): 快排

---
+++ Algorithm/排序/快速排序/quickSort.ts
@@ -0,0 +1,21 @@
+/**
+ * 快速排序
+ * @param {Array<number>} sortArray 
+ * @param {Number} low 
+ * @param {Number} hight 
+ */
+function quickSort(sortArray: number[], low?: number, hight?: number) {
+  if(low >= hight) return; // 左右指针相等，退出排序
+  // 指定左右指针
+  let i = low, j = hight;
+  let key = sortArray[low]; // 用子表的第一个记录做基准
+  while (i < j) { // 从表的两端交替向中间扫描
+      while (i < j && sortArray[j] >= key) j--;
+      if (i < j) sortArray[i++] = sortArray[j];// 用比基准小的记录替换低位记录
+      while (i < j && sortArray[i] < key) i++;
+      if (i < j) sortArray[j--] = sortArray[i]; // 用比基准大的记录替换高位记录
+  }
+  sortArray[i] = key;// 将基准数值替换回 sortArray[i]
+  quickSort(sortArray, low, i - 1); // 对低子表进行递归排序
+  quickSort(sortArray, j + 1, hight); // 对高子表进行递归排序
+}
