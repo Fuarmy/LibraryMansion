1ccd3ac5f23fd90bed9335b1d1b122a331662ebc
Diablo <750108747@qq.com>
2021-01-07 11:01:46 +0800

    feat(CSS3): CSS 特效之3D文字旋转 1. 可以使用鼠标控制旋转 2. 可以自动旋转

---
+++ Web/CSS 特效/3D 文字旋转/index.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  Splitting();
+}
+let circle = document.querySelector(".circle");
+/**
+ * 鼠标移动
+ * @param {} e
+ */
+window.onmousemove = function(e) {
+  let x = e.clientX/-2;
+  circle.style.animation = "none";
+  circle.style.transform = `perspective(1000px) rotateY(${x}deg) rotateX(15deg)`;
+}
+/**
+ * 鼠标移出
+ * @param {*} e
+ */
+window.onmouseout = function(e) {
+  circle.style.animation = "animate 8s linear infinite";
+}
